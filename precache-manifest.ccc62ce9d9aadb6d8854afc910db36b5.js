a95000b021a249e0ee169e500a17e0c2c94fedaf
LennonX0904 <LennonX0904@gmail.com>
2019-06-16 23:52:06 +0800

    Updates

---
+++ precache-manifest.ccc62ce9d9aadb6d8854afc910db36b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a6645a8c17d471da4db4dae26b0494",
+    "url": "/react-kanban/index.html"
+  },
+  {
+    "revision": "15d213c318a2e692fc18",
+    "url": "/react-kanban/static/css/main.d2c6feec.chunk.css"
+  },
+  {
+    "revision": "fbdd600d76cfa86be4dc",
+    "url": "/react-kanban/static/js/2.fc7831cb.chunk.js"
+  },
+  {
+    "revision": "15d213c318a2e692fc18",
+    "url": "/react-kanban/static/js/main.381d7aae.chunk.js"
+  },
+  {
+    "revision": "2552bfe471c686e65fe7",
+    "url": "/react-kanban/static/js/runtime~main.57763009.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-kanban/precache-manifest.83fbf3848343879162cc1a8fbaf0225e.js"
+  "/react-kanban/precache-manifest.ccc62ce9d9aadb6d8854afc910db36b5.js"
 );
 
 self.addEventListener('message', (event) => {
