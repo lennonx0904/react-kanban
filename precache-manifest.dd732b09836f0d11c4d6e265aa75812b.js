5769d4a581d8aa8b7a381851b1157a3cdaf8c400
LennonX0904 <LennonX0904@gmail.com>
2019-06-16 22:47:48 +0800

    Updates

---
+++ precache-manifest.dd732b09836f0d11c4d6e265aa75812b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577302b2c6b684b85ea33f4c1a1ee824",
+    "url": "/react-kanban/index.html"
+  },
+  {
+    "revision": "3153a68f1ad642fab63a",
+    "url": "/react-kanban/static/css/main.15a1b052.chunk.css"
+  },
+  {
+    "revision": "30fcf5c97586adb56373",
+    "url": "/react-kanban/static/js/2.7d2ac3ad.chunk.js"
+  },
+  {
+    "revision": "3153a68f1ad642fab63a",
+    "url": "/react-kanban/static/js/main.6a6b5798.chunk.js"
+  },
+  {
+    "revision": "2552bfe471c686e65fe7",
+    "url": "/react-kanban/static/js/runtime~main.57763009.js"
+  }
+]);
