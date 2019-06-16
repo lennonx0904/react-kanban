1a5715784ce3febdb89b2202a9fb2c80c22de073
LennonX0904 <LennonX0904@gmail.com>
2019-06-16 23:51:28 +0800

    update UI

--- src/component/Backlog/Title.js
+++ src/component/Backlog/Title.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { showNewCard } from "../../actions";
+import plus from "../../img/plus.png";
 
 class Title extends React.Component {
   render() {
     return (
       <div className="backlog-title ">
-        <div className="backlog-title-text">Backlog</div>
-        <div
-          className="backlog-title-text"
-          onClick={() => {
-            this.props.showNewCard(true);
-          }}
-        >
-          Add New Card
+        <div className="backlog-title-text">
+          Backlog
+          <img
+            className="add-card-mobile"
+            src={plus}
+            alt=""
+            onClick={() => {
+              this.props.showNewCard(true);
+            }}
+          />
         </div>
       </div>
     );
